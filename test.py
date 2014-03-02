4d917ce2cbb3ce60ee43ad8dc180a53eda2cd20d
Nathan <nathanl2012@gmail.com>
2014-03-02 07:24:50 -0500

    test.py

---
+++ test.py
@@ -0,0 +1,5 @@
+import json, requests
+url = "http://127.0.0.1:5000/request"
+data = {'sender': 'Alice', 'receiver': 'Bob', 'message': 'We did it!'}
+headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+r = requests.post(url, data=json.dumps(data), headers=headers)
